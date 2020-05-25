b4fdf7730899b74f1c2499f828792816991747eb
Daniel Winsvold <danielwinsvold@gmail.com>
2020-05-25 13:51:43 +0200

    fjerner serializers intill videre, bør evenutelt innføre serializers for alle styles, venter med det til vi ser behovet

--- components/BlockContent.tsx
+++ components/BlockContent.tsx
@@ -1,27 +1,8 @@
 import * as React from 'react';
 import SanityBlockContent from '@sanity/block-content-to-react';
-import { H2Header } from './styledComponents';
-
-const BlockRenderer = (props) => {
-  const { style = 'normal' } = props.node;
-
-  switch (style) {
-    case 'h2':
-      return <H2Header>{props.node.children[0].text}</H2Header>;
-    default:
-      return SanityBlockContent.defaultSerializers.types.block(props);
-  }
-};
-
-const serializers = {
-  types: {
-    block: BlockRenderer,
-    GtilNOK: (props) => props.node.G,
-  },
-};
 
 function BlockContent(props: { blocks: any }) {
-  return <SanityBlockContent serializers={serializers} blocks={props.blocks} />;
+  return <SanityBlockContent blocks={props.blocks} />;
 }
 
 export default BlockContent;

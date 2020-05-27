50f58b278bb46e28a87caa9ed4faf213812e1330
Daniel Winsvold <danielwinsvold@gmail.com>
2020-05-27 13:02:21 +0200

    mulighet for å legge relatert informasjon i liste, det gir bedre muligheter for styling i frontend

--- schemas/relatertInformasjonRichText.js
+++ schemas/relatertInformasjonRichText.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import externalLink from './annotations/externalLink';
 import internalLink from './annotations/internalLink';
-import { blocks, decorators, styles } from './richText';
+import { decorators, styles } from './richText';
 import visForAnnotation from './annotations/visForAnnotation';
 
 export default {
   name: 'relatertInformasjonRichText',
   type: 'array',
+  description: 'Legg gjerne lenkene i en bulletpoint-liste, da blir formatteringen i frontend bedre',
   of: [
     {
       title: 'Block',
       type: 'block',
       styles: [styles.normal],
-      lists: [],
+      lists: [{ title: 'Bullet', value: 'bullet' }],
       marks: {
         decorators: [decorators.utkast],
         annotations: [externalLink, internalLink, visForAnnotation],

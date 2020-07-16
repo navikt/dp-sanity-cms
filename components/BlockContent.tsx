b2dbc7266f0ef1fe6eda839e22c4db8ad9ed8aac
Daniel Winsvold <danielwinsvold@gmail.com>
2020-07-16 15:27:42 +0200

    rendring av GtilNOK i richTextPreview

--- components/BlockContent.tsx
+++ components/BlockContent.tsx
@@ -1,8 +1,25 @@
 import * as React from 'react';
+import { ReactNode } from 'react';
 import SanityBlockContent from '@sanity/block-content-to-react';
+import { GtilNOKPeview } from '../schemas/objects/GtoNOK';
+type Serializers = {
+  types: {
+    [key: string]: ({ node: any }) => ReactNode;
+  };
+  marks: {
+    [key: string]: any;
+  };
+};
+
+const serializers: Serializers = {
+  types: {
+    GtilNOK: (props) => <GtilNOKPeview G={props.node.G} />,
+  },
+  marks: {},
+};
 
 function BlockContent(props: { blocks: any }) {
-  return <SanityBlockContent blocks={props.blocks} />;
+  return <SanityBlockContent blocks={props.blocks} serializers={serializers} />;
 }
 
 export default BlockContent;
--- schemas/objects/GtoNOK.js
+++ schemas/objects/GtoNOK.js
@@ -1,10 +1,31 @@
+import React from 'react';
 import Money from 'react-icons/lib/fa/money';
+import styled from 'styled-components';
+
+export const GtilNOKIcon = Money;
+
+const Style = styled.span`
+  background-color: limegreen;
+  padding: 0.1rem 0.3rem;
+  > * {
+    margin-right: 0.2rem;
+  }
+`;
+
+export const GtilNOKPeview = (props) => {
+  return (
+    <Style>
+      <Money />
+      {props.G}G
+    </Style>
+  );
+};
 
 export default {
   name: 'GtilNOK',
   type: 'object',
   title: 'Grunnbeløp til kroner',
-  icon: Money,
+  icon: GtilNOKIcon,
   fields: [
     {
       type: 'number',
@@ -13,4 +34,11 @@ export default {
       description: 'Grunnbeløpet må oppdateres i frontenden ved G-justering.',
     },
   ],
+  preview: {
+    select: {
+      G: 'G',
+    },
+    prepare: (selection) => selection,
+    component: (props) => <GtilNOKPeview G={props.value.G} />,
+  },
 };

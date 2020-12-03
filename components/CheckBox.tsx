2e66962b4c9fa0c24b1f67dfbeb601660d910c5f
Celine Dam <celine.ten.dam@nav.no>
2020-12-03 11:50:04 +0100

    Stylet checkboxene
    Co-authored-by: Daniel Winsvold <daniel.winsvold@nav.no>

--- components/CheckBox.tsx
+++ components/CheckBox.tsx
@@ -1,6 +1,7 @@
 import { useRef } from 'react';
 import { guid } from 'nav-frontend-js-utils';
 import * as React from 'react';
+import styled from 'styled-components';
 
 interface CheckBoxProps {
   label: string;
@@ -8,13 +9,23 @@ interface CheckBoxProps {
   onChange: () => void;
 }
 
+const StyledInput = styled.input`
+  height: 1.4rem;
+  width: 1.4rem;
+  margin-right: 0.5rem;
+`;
+const Style = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 0.5rem;
+`;
 function CheckBox(props: CheckBoxProps) {
   const id = useRef(guid()).current;
   return (
-    <div>
-      <input id={id} type="checkbox" value={props.label} checked={props.checked} onChange={props.onChange} />
+    <Style>
+      <StyledInput id={id} type="checkbox" value={props.label} checked={props.checked} onChange={props.onChange} />
       <label htmlFor={id}>{props.label}</label>
-    </div>
+    </Style>
   );
 }
 

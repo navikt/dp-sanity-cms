eac4731bef362dac6465e984b2cd0e11c8a42770
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2023-02-27 14:19:35 +0100

    Skjule mine dagpenger schema fra innhold meny

--- desk-structure.ts
+++ desk-structure.ts
@@ -344,6 +344,7 @@ export default () =>
             ...soknadSchemaNames,
             ...oldSchemaNames,
             ...innsynSchemaNames,
+            ...mineDagpengerSchemaNames,
           ].includes(<string>listItem.getId())
       ),
     ]);
